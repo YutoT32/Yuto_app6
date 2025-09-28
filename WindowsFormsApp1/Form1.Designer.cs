c37d866bf1acebe812057aa814c4c0196f15cbae
yuto <warabi2mochi@gmail.com>
2025-09-28 14:37:26 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(52, 40);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(79, 15);
+            this.lblInput.TabIndex = 0;
+            this.lblInput.Text = "入力ファイル";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(52, 120);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(79, 15);
+            this.lblOutput.TabIndex = 1;
+            this.lblOutput.Text = "出力ファイル";
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(639, 73);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(75, 23);
+            this.btnInput.TabIndex = 2;
+            this.btnInput.Text = "参照";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(639, 148);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnOutput.TabIndex = 3;
+            this.btnOutput.Text = "参照";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(55, 73);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(549, 22);
+            this.txtInput.TabIndex = 4;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(55, 148);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(549, 22);
+            this.txtOutput.TabIndex = 5;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(235, 209);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 6;
+            this.btnExecute.Text = "実行";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(442, 209);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 7;
+            this.btnEnd.Text = "終了";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnOutput);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblInput);
+            this.Name = "frmMain";
+            this.Text = "実行画面";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnEnd;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 入力側「参照」ボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            //ダイアログボックスのインスタンスを生成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //ダイアログボックスのプロパティ設定
+            ofd.Title = "入力ファイルを選択してください。";
+            ofd.InitialDirectory = @"C:\";
+            ofd.Filter = "text(*.txt)|*.txt";
+
+            //ファイル選択ダイアログボックス表示
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtInput.Text = ofd.FileName;
+            }
+            else
+            {
+                txtInput.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// 出力側「参照」ボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            //ダイアログボックスのインスタンスを生成
+            SaveFileDialog ofd = new SaveFileDialog();
+
+            //ダイアログボックスのプロパティ設定
+            ofd.Title = "出力ファイルを選択してください。";
+            ofd.InitialDirectory = @"C:\";
+            ofd.Filter = "text(*.txt)|*.txt";
+
+            //ファイル選択ダイアログボックス表示
+            if ( ofd.ShowDialog() == DialogResult.OK )
+            {
+                txtOutput.Text = ofd.FileName;
+            }
+            else
+            {
+                txtOutput.Text = "";
+            }
+        }
+    }
+}
